98d250af48a666e7326b78a9b7ca3a864d8508c8
TITANFR <titanfrmax@gmail.com>
2025-04-27 17:08:00 +0200

    Construction du dossier Router et création du fichier index le squelette des différentes pages et 2 pages 404 et home

---
+++ Router/allRoutes.js
@@ -0,0 +1,9 @@
+import Route from "./Route.js";
+
+//Définir toutes nos routes
+export const allRoutes = [
+    new Route("/" , "Accueil", "/Pages/home.html"),
+];
+
+//Le titre s'affiche comme ceci : Route.titre = websitename
+export const websiteName = "ECORIDE";
