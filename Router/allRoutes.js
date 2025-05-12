18809c6576a12d16f2f25c91e60a115858de964a
TITANFR <titanfrmax@gmail.com>
2025-05-12 18:04:52 +0200

    fichier html header home covoiturage et js covoiturage. router et css

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,8 @@ import Route from "./Route.js";
 //Définir toutes nos routes
 export const allRoutes = [
     new Route("/" , "Accueil", "/Pages/home.html"),
+    new Route("/covoiturage" , "Covoiturage", "/Pages/covoiturage.html", "/JS/covoiturage.js"),
+    new Route("/contact" , "Contact", "/Pages/contact.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre = websitename
